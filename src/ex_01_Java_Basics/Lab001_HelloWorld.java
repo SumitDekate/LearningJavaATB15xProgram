fc34ae2f494a73a0c1b69d1da9167f6dab8f16b6
Sumit Dekate <sumitdekate14@gmail.com>
2026-01-07 00:10:12 +0530

    Initial Commit

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorld {
     static void main() {
         System.out.println("HelloWorld");
+        System.out.println("Hello Java World");
     }
 }
